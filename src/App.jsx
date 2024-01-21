f3d276505a70ed3e7076b87fb8c6216cb6f19ed3
Syed Maruf Ali <syedmarufali23@gmail.com>
2024-01-21 17:31:41 +0530

    Search Done

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,38 @@ import React, { useState } from "react";
 import "./App.scss";
 
 // Components Imports
-import {Users} from "./users";
+import { Users } from "./users";
 import Table from "./Table";
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
-  console.log(searchTerm)
+  console.log(searchTerm);
 
   const handleChange = (event) => {
     setSearchTerm(event.target.value);
-  }
+  };
+
+  const keys = ["first_name", "last_name", "email"];
+
+
+  // Search Function
+  const Search = (data) => {
+    return data.filter((item) =>
+      keys.some((key) =>
+        item[key].toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    );
+  };
   return (
     <div className="app">
-      <input type="text" placeholder="Search.." className="search" onChange={handleChange}  />
+      <input
+        type="text"
+        placeholder="Search.."
+        className="search"
+        onChange={handleChange}
+      />
       <ul className="list">
-        <Table data={Users} />
+        <Table data={Search(Users)} />
       </ul>
     </div>
   );

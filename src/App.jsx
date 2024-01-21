033467ff04e2ec85adecdea2e2536f7aa28a1ae9
Syed Maruf Ali <syedmarufali23@gmail.com>
2024-01-21 16:26:09 +0530

    Prj Initialized

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import './App.scss'
+import React from "react";
+import "./App.scss";
+
+// Users Data
+import { Users } from "./users";
 
 const App = () => {
   return (
-    <div className="App">Sdmrf</div>
-  )
-}
+    <div className="app">
+      <input type="text" placeholder="Search.." className="search" />
+      <ul className="list">
+        {Users.map((user) => (
+          <li className="listItem">{user.first_name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default App
+export default App;

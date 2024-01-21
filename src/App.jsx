ea2bb4938a45cc654d8b98583e47f81566c7cd5b
Syed Maruf Ali <syedmarufali23@gmail.com>
2024-01-21 16:57:49 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.scss";
 
-// Users Data
-import { Users } from "./users";
+// Components Imports
+import {Users} from "./users";
+import Table from "./Table";
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  console.log(searchTerm)
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  }
   return (
     <div className="app">
-      <input type="text" placeholder="Search.." className="search" />
+      <input type="text" placeholder="Search.." className="search" onChange={handleChange}  />
       <ul className="list">
-        {Users.map((user) => (
-          <li className="listItem">{user.first_name}</li>
-        ))}
+        <Table data={Users} />
       </ul>
     </div>
   );
